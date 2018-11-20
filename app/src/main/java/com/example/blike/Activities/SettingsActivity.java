d8398d662e5b2c4c2482ae85439e4d659d01cdfa
Albert Fernandez <albertfernandezgarcia@gmail.com>
2018-11-20 17:02:39 +0100

    bLIKE primer commit 20 nov+

--- app/src/main/java/com/example/blike/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/blike/Activities/SettingsActivity.java
@@ -87,7 +87,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return true;
         }
     };
-
+//
     /**
      * Helper method to determine if the device has an extra-large screen. For
      * example, 10" tablets are extra-large.
